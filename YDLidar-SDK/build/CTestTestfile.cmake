238ea7c131ced100bb2b08784b184c2c97d35efa
jeonsion <a24349663@gmail.com>
2024-04-15 02:47:36 +0900

    motor_recovery

--- YDLidar-SDK/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sion-lp/ros_ws/src/Olaf/YDLidar-SDK
-# Build directory: /home/sion-lp/ros_ws/src/Olaf/YDLidar-SDK/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("core")
-subdirs("src")
-subdirs("examples")

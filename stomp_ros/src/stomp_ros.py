031da173ee5dd22555734c51a7ab18c919442ede
jeonsion <a24349663@gmail.com>
2024-05-29 04:36:09 +0900

    update

--- stomp_ros/src/stomp_ros.py
+++ stomp_ros/src/stomp_ros.py
@@ -9,14 +9,15 @@ from geometry_msgs.msg import PointStamped
 
 # 매핑 테이블: 문자 -> 좌표 (x, y)
 click_point_mapping = {
-    "53": (7.581643104553223, 7.222347259521484),
-    "54": (5.386044502258301, 8.285445213317871),
-    "55": (10.152769088745117, 9.670970916748047)
+    "53": (7.67016077041626, 6.969274044036865),
+    "1": (15.101937294006348, 6.4472761154174805),
+    "2": (14.799625396728516, 4.137206077575684)
 }
 
 # ROS 노드 초기화
 rospy.init_node('STOMP_ROS')
-pub = rospy.Publisher('/clicked_point', PointStamped, queue_size=10)
+start_pub = rospy.Publisher('/start_point', PointStamped, queue_size=10)
+end_pub = rospy.Publisher('/end_point', PointStamped, queue_size=10)
 
 async def connect():
     ws_url = f"ws://{args.host}:{args.port}/ws"
@@ -40,35 +41,46 @@ async def connect():
             body = parsed_message['body']
             try:
                 data = json.loads(body)
-                if "startPoint" in data:
-                    start_point = data["startPoint"]
-                    end_point = data["endPoint"]
+                if "startPoint" in data and "endPoint" in data:
+                    start_point = str(data["startPoint"])
+                    end_point = str(data["endPoint"])
                     print(f"User {start_point}에서 User {end_point}로 전달합니다.")
-                else:
-                    user_location = data.get("userLocation")
+
+                    x, y = click_point_mapping[start_point]
+                    publish_click_point(x, y, 'start')
+                    x, y = click_point_mapping[end_point]
+                    publish_click_point(x, y, 'end')
+                elif "bookLocation" in data and "userLocation" in data:
+                    book_location = str(data["bookLocation"])
+                    user_location = str(data["userLocation"])
                     print(f"User {user_location}이 책을 대여했습니다.")
-
-                location_info = data.get("bookLocation")
-                if location_info in click_point_mapping:
-                    x, y = click_point_mapping[location_info]
-                    publish_click_point(x, y)
-                # else:
-                #     # print(f"Unknown locationInfo: {location_info}")
+                    x, y = click_point_mapping[book_location]
+                    publish_click_point(x, y, 'start')
+                    x, y = click_point_mapping[user_location]
+                    publish_click_point(x, y, 'end')
+                    
+                else:
+                    print(f"Unknown message format: {data}")
             except json.JSONDecodeError as e:
-                print(f"Prepare to receive message: {body}")
+                print(f"Prepare to Receive Message")
 
-def publish_click_point(x, y):
-    # 현재 시간을 기준으로 PointStamped 메시지 생성
+def publish_click_point(x, y, point_type):
     point_msg = PointStamped()
     point_msg.header.stamp = rospy.Time.now()
-    point_msg.header.frame_id = "map"  # 필요한 프레임 아이디로 변경
+    point_msg.header.frame_id = "map"
     point_msg.point.x = x
     point_msg.point.y = y
     point_msg.point.z = 0.0
 
-    # print(f"Publishing point: {point_msg}")
-    # 토픽 발행
-    pub.publish(point_msg)
+    if point_type == 'start':
+        print(f"Publishing start point: {point_msg}")
+        start_pub.publish(point_msg)
+    elif point_type == 'end':
+        print(f"Publishing end point: {point_msg}")
+        end_pub.publish(point_msg)
+    else:
+        proint(f"Unknown point type: {point_type}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="STOMP Client")
